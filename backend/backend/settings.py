cee5fcbfb6c47bbbf165fe522e3a14ff1e2fb172
Metehan Yıldırım <44446768+metegenez@users.noreply.github.com>
2021-11-01 00:07:32 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,15 +39,15 @@ CORS_ALLOW_HEADERS = ['*']
 # SECURITY WARNING: don't run with debug turned on in production!
 
 APPEND_SLASH = False
-print(os.environ.get("db_username"))
-print("-------")
-print(os.environ.get("secret"))
+
 try:
     secret = json.loads(os.environ.get("secret"))
-    print(secret)
 except:
-    print("olmadı")
-    secret = {}
+    try:
+        secret = json.loads(os.environ.get("db_username")) #Due to ASM misconfiguration on Task Definition.
+    except:
+        secret = {}
+
 DEBUG = True if os.environ.get("STAGE") == "dev" or os.environ.get("STAGE") == "local" else False
 db_username = env("db_username") if os.environ.get("STAGE") == "local" else secret["db_username"]
 db_password = env("db_password") if os.environ.get("STAGE") == "local" else secret["db_password"]
